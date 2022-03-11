2e2ec822782a8d019842670f7e889d6a3b96929d
Bruno Pinho <brunopinho@outlook.com>
2022-03-11 16:50:34 -0300

    feat: create index.js and express initial setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3001;
+
+app.get('/', (_req, res) => res.send(`Hello World!`));
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}/`));
