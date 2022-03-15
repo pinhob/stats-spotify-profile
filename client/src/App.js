f190a18bc326841de5d640dd1f259be4184c975c
Bruno Pinho <brunopinho@outlook.com>
2022-03-15 16:16:30 -0300

    feat: set react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from 'react-router-dom';
 import './App.css';
 import { accessToken, logout, getCurrentUserProfile } from './spotify';
 import { catchErrors } from './utils';
+
+// Scroll to top of page when changing routes
+// https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   const [token, getToken] = useState(null);
@@ -32,18 +50,39 @@ function App() {
               </a>
             )
             : (
-              <main>
-                <button onClick={logout}>Log Out</button>
-                { profile && (
-                  <div>
-                    <h1>{ profile.display_name }</h1>
-                    <p>{ `${ profile.followers.total } followers` }</p>
-                    {profile.images.length && profile.images[0].url && (
-                      <img src={profile.images[0].url} alt="Avatar"/>
-                    )}
-                  </div>
-                ) }
-              </main>
+              <Router>
+                <ScrollToTop />
+
+                <Switch>
+                  <Route path="/top-artists">
+                    <h1>Top Artists</h1>
+                  </Route>
+                  <Route path="/top-tracks">
+                    <h1>Top Tracks</h1>
+                  </Route>
+                  <Route path="/playlists/:id">
+                    <h1>Playlist</h1>
+                  </Route>
+                  <Route path="/playlists">
+                    <h1>Playlists</h1>
+                  </Route>
+                  <Route path="/">
+                    <>
+                      <button onClick={logout}>Log Out</button>
+    
+                      {profile && (
+                        <div>
+                          <h1>{profile.display_name}</h1>
+                          <p>{profile.followers.total} Followers</p>
+                          {profile.images.length && profile.images[0].url && (
+                            <img src={profile.images[0].url} alt="Avatar"/>
+                          )}
+                        </div>
+                      )}
+                    </>
+                  </Route>
+                </Switch>
+              </Router>
             )
         }
       </header>
